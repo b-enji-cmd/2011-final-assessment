de160a9d75dae356e23940986b0969ef260c0e7d
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-08 11:06:32 -0700

    Update routing and write surgery show form

--- app/controllers/surgeries_controller.rb
+++ app/controllers/surgeries_controller.rb
@@ -1,9 +1,26 @@
 class SurgeriesController < ApplicationController
+	before_action :find_model
 	def show
-		@surgery = Surgery.find(params[:id])
 		@doctors = DoctorSurgery.all.where(surgery_id: @surgery.id)
 		@doctors.each do |doctor|
 			@surgery.doctors << Doctor.find(doctor.doctor_id)
 		end
+	end
+
+	def update
+		@doctor = Doctor.find_by name: params[:name]
+		@surgery.doctors << @doctor
+
+		redirect_to surgery_path(@surgery)
+	end
+
+	private
+
+	def surgery_params
+		params.permit(:name, :id)
+	end
+
+	def find_model
+		@surgery = Surgery.find(params[:id])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
 	resources :doctors, only: [:index]
-	resources :surgeries, only: [:show]
+	resources :surgeries, only: [:show, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
