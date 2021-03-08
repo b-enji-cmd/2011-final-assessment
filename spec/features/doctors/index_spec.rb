12463670c5d9ea98176011085f3da9fcb78099e6
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-08 09:37:01 -0700

    Write doctor index feature test

--- spec/features/doctors/index_spec.rb
+++ spec/features/doctors/index_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe 'doctor index' do
 	end
 
 	it "can see all doctors info" do
+		visit doctor_index_path(@doctor_1)
 		within ("#doctor-#{@doctor_1.id}") do
 			expect(page).to have_content("#{@doctor_1.name}")
+			expect(page).to have_content("#{@doctor_1.years_practiced}")
+			expect(page).to have_content("#{@doctor_1.uni}")
 		end
 	end
 end
