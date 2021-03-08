26a3533ecd200d7f148cfc32dc8d57a9f6a152c2
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-08 10:37:00 -0700

    Write model instance method tests for surgery

--- spec/models/surgery_spec.rb
+++ spec/models/surgery_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Surgery, type: :model do
 		@doctor_1 = Doctor.create!(name: 'John', years_practiced: 30, uni: 'Case Western')
 		@doctor_2 = Doctor.create!(name: 'Mary', years_practiced: 40, uni: 'Anchutz')
 		@surgery_1 = Surgery.create!(title: 'Heart Transplant', day: 'Tuesday', room: 113 )
-		DoctorSurgery.create!(doctor_id: @doctor_1.id, surgery_id: @surgery_1.id)
-		DoctorSurgery.create!(doctor_id: @doctor_2.id, surgery_id: @surgery_1.id)
+		@surgery_1.doctors << @doctor_1
+		@surgery_1.doctors << @doctor_2
 	end
   describe "validations" do
     it { should validate_presence_of :title }
@@ -25,9 +25,9 @@ RSpec.describe Surgery, type: :model do
   	end
 
   	it "#worst_doctor" do
-  		expect(@surgery_1.best_doctor.class).to eq(Hash)
-  		expect(@surgery_1.best_doctor[:name]).to eq("John")
-  		expect(@surgery_1.best_doctor[:years]).to eq(30)
+  		expect(@surgery_1.worst_doctor.class).to eq(Hash)
+  		expect(@surgery_1.worst_doctor[:name]).to eq("John")
+  		expect(@surgery_1.worst_doctor[:years]).to eq(30)
   	end
   end
 end
