bb6f2a1101a169023e805e01713e428bca750d76
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-08 09:31:07 -0700

    Surgery and DoctorSurgery model tests

---
+++ spec/models/surgery_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Surgery, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :day }
+    it { should validate_presence_of :room }
+  end
+  describe "relationships" do
+    it { should have_many :doctors }
+  end
+end
