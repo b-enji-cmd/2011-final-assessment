be9318bc06c82c5b00f0e83c8ef0766c58cac0fc
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-08 10:00:17 -0700

    Write surgery show spec tests

---
+++ spec/features/surgeries/show_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'surgery show page' do
+	before :each do
+		@doctor_1 = Doctor.create!(name: 'John', years_practiced: 30, uni: 'Case Western')
+		@doctor_2 = Doctor.create!(name: 'Mary', years_practiced: 40, uni: 'Anchutz')
+		@surgery_1 = Surgery.create!(title: 'Heart Transplant', day: 'Tuesday', room: 113 )
+		DoctorSurgery.create!(doctor_id: @doctor_1.id, surgery_id: @surgery_1.id)
+		DoctorSurgery.create!(doctor_id: @doctor_2.id, surgery_id: @surgery_1.id)
+		visit surgery_path(@surgery_1)
+	end
+
+	it "displays the surgery info" do
+		expect(page).to have_content("#{@surgery_1.title}")
+		expect(page).to have_content("#{@surgery_1.day}")
+		expect(page).to have_content("#{@surgery_1.room}")
+	end
+
+	it "has two sections for least and most experienced doctors" do
+		within ("#most-experienced") do
+			expect(page).to have_content("#{@doctor_2.name}")
+			expect(page).to have_content("#{@doctor_2.years_practiced}")
+		end
+
+		within ("#least-experienced") do
+			expect(page).to have_content("#{@doctor_1.years_practiced}")
+		end
+	end
+end
