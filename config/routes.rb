6e76ab742ba29a4d2a07e7230fff0f8e7c4e0d61
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-08 22:39:27 -0700

    Update routing to feature join table controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 	resources :doctors, only: [:index]
-	resources :surgeries, only: [:show, :update, :index]
+	resources :surgeries, only: [:show, :index]
+	resources :doctor_surgeries, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
