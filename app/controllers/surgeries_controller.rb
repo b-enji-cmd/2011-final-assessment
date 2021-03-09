0cce11f278b4c29dc7706ce22449874910c5021c
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-09 10:23:02 -0700

    Final refactor

--- app/controllers/surgeries_controller.rb
+++ app/controllers/surgeries_controller.rb
@@ -6,6 +6,7 @@ class SurgeriesController < ApplicationController
 
 	def show
 		@surgery = Surgery.find(params[:id])
+		@presiding_doctors = @surgery.doctors
 		@least_experienced = @surgery.worst_doctor
 		@most_experienced = @surgery.best_doctor
 	end
